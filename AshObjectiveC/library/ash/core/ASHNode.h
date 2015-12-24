7abfe7fda8bd3074a219c5b90868d6150e8a0df0
Igor Kravchenko <igman2005@gmail.com>
2015-12-24 15:18:40 +0200

    One more nullability update for ASHNode

--- AshObjectiveC/library/ash/core/ASHNode.h
+++ AshObjectiveC/library/ash/core/ASHNode.h
@@ -10,13 +10,9 @@
     __strong ASHNode  * __nullable next;
 }
 
+@property (nullable, nonatomic, strong) ASHNode  * next;
 @property (nonnull, nonatomic, readonly) ASHEntity * entity;
-
 @property (nullable, nonatomic, weak) ASHNode * previous;
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wnullability-completeness"
-@property (nullable, nonatomic, strong) ASHNode  * next;
-#pragma clang diagnostic pop
 
 @end
